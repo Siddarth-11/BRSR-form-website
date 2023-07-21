4221d14d5723d78456d47f408bf680832725c36a
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2023-07-21 08:42:23 +0400

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, text
-#import os
-#my_secret = os.environ['DB_CONNECTION_STR']
-db_connection_string = "mysql+pymysql://v4roct5mdaikueazsv8c:pscale_pw_oRpbtfmznSeDuNE8Tn4OToU82crlTFXSsQobtsyhJQw@aws.connect.psdb.cloud/brsr_form?charset=utf8mb4"
+import os
+
+db_connection_string = os.environ['db_connection_string']
 
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
@@ -11,40 +11,67 @@ engine = create_engine(db_connection_string,
 with engine.connect() as conn:
   result = conn.execute(text("select * from brsr_form.listed_entity"))
 
-result_dicts = []
-for row in result.all():
-  result_dicts.append(list(row))
+  result_dicts = []
+  for row in result.all():
+    result_dicts.append(list(row))
 
-# print(result_dicts)
-"""
-def add_to_db(data):
+  print(result_dicts)
+
+def listed_entity_data(info):
   with engine.connect() as conn:
-    query = text("INSERT INTO listed_entity(cin, name, year_of_incorporation, registered_office_address, corporate_address, email, telephone, website, financial_year, stock_exchange, paid_up_capital, contact_person_name, contact_person_telephone, contact_person_email, reporting_boundary) VALUES(:cin, :name, :year_of_incorporation, :registered_office_address, :corporate_address, :email, :telephone, :website, :financial_year, :stock_exchange, :paid_up_capital, :contact_person_name, :contact_person_telephone, :contact_person_email, :reporting_boundary)")
+    query = text("INSERT INTO listed_entity(cin, year_of_incorporation, corporate_address, email, telephone, website, financial_year, stock_exchange, paid_up_capital, contact_person_telephone, contact_person_email, reporting_boundary) VALUES(:cin, :year_of_incorporation, :corporate_address, :email, :telephone, :website, :financial_year, :stock_exchange, :paid_up_capital, :contact_person_telephone, :contact_person_email, :reporting_boundary)")
 
-    conn.execute(query,
-                cin = data['cin'],
-                name = data['name'],
-                year_of_incorporation = data['year_of_incorporation'],
-                registered_office_address = data['registered_office_address'],
-                corporate_address = data['corporate_address'],
-                email = data['email'],
-                telephone = data['telephone'],
-                website = data['website'],
-                financial_year = data['financial_year'],
-                stock_exchange = data['stock_exchange'],
-                paid_up_capital = data['paid_up_capital'],
-                contact_person_name = data['contact_person_name'],
-                contact_person_telephone = data['contact_person_telephone'],
-                contact_person_email = data['contact_person_email'],
-                reporting_boundary = data['reporting_boundary']
-                )
-
-"""
+    conn.execute(query,{
+                 "cin" : info['cin'],
+                 "year_of_incorporation" : info['year_of_incorporation'],
+                 "corporate_address" : info['corporate_address'],
+                 "email" : info['email'],
+                 "telephone" : info['telephone'],
+                 "website" : info['website'],
+                 "financial_year" : info['financial_year'],
+                 "stock_exchange" : info['stock_exchange'],
+                 "paid_up_capital" : info['paid_up_capital'],
+                 "contact_person_telephone" : info['contact_person_telephone'],
+                 "contact_person_email" : info['contact_person_email'],
+                 "reporting_boundary" : info['reporting_boundary']})
 
 
+def bussiness_activites(info):
+  with engine.connect() as conn:
+    query = text("INSERT INTO BusinessActivities (DescriptionOfMainActivity_1,DescriptionOfBusinessActivity_1,TurnoverPercentage_1,DescriptionOfMainActivity_2,DescriptionOfBusinessActivity_2,TurnoverPercentage_2) VALUES (:DescriptionOfMainActivity_1,:DescriptionOfBusinessActivity_1,:TurnoverPercentage_1,:DescriptionOfMainActivity_2,:DescriptionOfBusinessActivity_2,:TurnoverPercentage_2)")
+
+    conn.execute(query , {
+     "DescriptionOfMainActivity_1" : info['main_Activity_1'] ,
+    "DescriptionOfBusinessActivity_1" : info['bussiness_Acitvity_1'] ,
+    "TurnoverPercentage_1" : info['turnover_1'] ,
+    "DescriptionOfMainActivity_2" : info['main_Activity_2'] ,
+    "DescriptionOfBusinessActivity_2" : info['bussiness_Acitvity_2'] ,
+    "TurnoverPercentage_2" : info['turnover_2']})
+
+def products_services(info):
+  with engine.connect() as conn:
+    query = text("insert into ProductsServices (ProductServiceDescription_1,NICCode_1 ,TurnoverPercentage_1 ,ProductServiceDescription_2 ,NICCode_2 ,TurnoverPercentage_2 ,ProductServiceDescription_3 ,NICCode_3 ,TurnoverPercentage_3) values (:ProductServiceDescription_1,:NICCode_1 ,:TurnoverPercentage_1 ,:ProductServiceDescription_2 ,:NICCode_2 ,:TurnoverPercentage_2 ,:ProductServiceDescription_3 ,:NICCode_3 ,:TurnoverPercentage_3)")
+
+    conn.execute(query , {
+     "ProductServiceDescription_1" : info['P/S_1'] ,
+    "NICCode_1" : info['nic_number_1'] ,
+    "TurnoverPercentage_1" : info['turnover_contributed_1'] ,
+    "ProductServiceDescription_2" : info['P/S_2'] ,
+    "NICCode_2" : info['nic_number_2'] ,
+    "TurnoverPercentage_2" : info['turnover_contributed_2'],
+    "ProductServiceDescription_3" : info['P/S_3'] ,
+    "NICCode_3" : info['nic_number_3'] ,
+    "TurnoverPercentage_3" : info['turnover_contributed_3']
+    })
+
+
+
+
+                 
 def load_data_from_db(id):
   with engine.connect() as conn:
-    result = conn.execute(text("select * from brsr_form.listed_entity where id = 1"))
+    result = conn.execute(
+      text("select * from brsr_form.listed_entity where id = 1"))
     rows = result.all()
     if len(rows) == 0:
       return None
@@ -55,9 +82,12 @@ def load_data_from_db(id):
 def store_trial_data(data):
   with engine.connect() as conn:
     query = text(
-      "INSERT INTO brsr_form.trial (Email_input, Tele_input, name_input) VALUES (siddarth, 12345, Sid)")
-    
-    conn.execute(query)
-                  #{"email_input" : data['Email_input']},
-                  #{"tele_input" : data['Tele_input']},
-                  #{"Name_input" : data['name_input']})
+      "INSERT INTO brsr_form.trial (Email_input, Tele_input, name_input) VALUES (:email, :tele, :name)"
+    )
+
+    conn.execute(
+      query, {
+        "email": data['Email_input'],
+        "tele": data['Tele_input'],
+        "name": data['name_input']
+      })
