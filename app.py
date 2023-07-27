dbb2a078656914c7f4433a9fd83d7bc7fa5f173b
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2023-07-27 22:31:04 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,15 @@
-from flask import Flask, render_template, jsonify, request
-from database import engine, load_data_from_db, store_trial_data
+from flask import Flask, render_template, request
+from database import listed_entity_data,bussiness_activites,products_services, plants_located_data, market_served_data, company_details_data,Comm_ComplaintsGrievances,Investers_ComplaintsGrievances,Shareholders_ComplaintsGrievances,Emp_ComplaintsGrievances,Customer_ComplaintsGrievances,partners_ComplaintsGrievances,others_ComplaintsGrievances,entity_overview_data, landing_page_data , insert_employee_data
 
 app = Flask(__name__)
-'''
-def load_from_db():
-  with engine.connect() as conn:
-    result = conn.execute("select * from brsr_form.listed_entity")
-    data = []
-    for row in result.all():
-      data.append(dict(row))
-      return data
-'''
-'''
-@app.route("/<id>")
-def load_db(id):
-  load = load_data_from_db(id)
-  return jsonify(load)
-'''
 
-
-@app.route(
-  "/", )
+@app.route("/")
 def main_page():
-  return render_template('mainpage.html')
+  return render_template('landing_page.html')
 
 
 @app.route("/DOE.html")
-def main_page1():
+def DOE():
   return render_template('mainpage.html')
 
 
@@ -60,23 +43,57 @@ def trial():
   return render_template("trial.html")
 
 
-# data = request.args
-# return jsonify(list(data))
+@app.route("/db", methods=['post'])
+def input0():
+  name = request.form.to_dict()
+  landing_page_data(name)
+  return render_template('landing_page.html', application=name)
 
 
 @app.route("/inputdb", methods=['post'])
-def input():
+def input1():
   name = request.form.to_dict()
-  store_trial_data(name)
-  return render_template('Form_submitted.html', application=name)
+  listed_entity_data(name)
+  return render_template('mainpage.html', application=name )
 
+@app.route("/productdb", methods=['POST'])
+def input2():
+  name = request.form.to_dict()
+  bussiness_activites(name)
+  products_services(name)
+  return render_template('products.html', application=name )
 
-'''
-@app.route("/input", methods=["POST" , "GET"])
-def send_to_db():
-  data = request.form
-  return jsonify(list(data))
-'''
+@app.route("/operationsdb", methods=['POST'])
+def input3():
+  name = request.form.to_dict()
+  plants_located_data(name)
+  market_served_data(name)
+  return render_template('operations.html', application=name )
+
+@app.route("/HSAdb", methods=['POST'])
+def input4():
+  name = request.form.to_dict()
+  company_details_data(name)
+  return render_template('HSA.html', application=name )
+
+@app.route("/TADdb", methods=['POST'])
+def input5():
+  name = request.form.to_dict()
+  Comm_ComplaintsGrievances(name)
+  Investers_ComplaintsGrievances(name)
+  Shareholders_ComplaintsGrievances(name)
+  Emp_ComplaintsGrievances(name)
+  Customer_ComplaintsGrievances(name)
+  partners_ComplaintsGrievances(name)
+  others_ComplaintsGrievances(name)
+  entity_overview_data(name)
+  return render_template('TAD.html', application=name )
+
+@app.route("/Employeesdb", methods=['POST'])
+def input6():
+  name = request.form.to_dict()
+  insert_employee_data(name)
+  return render_template('employees.html', application=name )
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
