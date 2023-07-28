062a3bb3c0893eb05b91d25308bc1bc106786329
Siddarth-11 <138587533+Siddarth-11@users.noreply.github.com>
2023-07-29 00:17:47 +0400

    Update database with new insert queries in tables

--- database.py
+++ database.py
@@ -366,4 +366,4 @@ def insert_employee_data(info):
             "PW_fy_year_prior_1": info["a82"],
             "PW_fy_year_prior_2": info["a83"],
             "PW_fy_year_prior_3": info["a84"]
-        }) 
+        })  
